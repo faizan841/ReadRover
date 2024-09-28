02bb9430bb32d3c18267f8062ea4cb33042c3109
faizan841 <faizan200620@gmail.com>
2024-09-29 00:44:39 +0530

    change

--- Backend/server.js
+++ Backend/server.js
@@ -6,23 +6,28 @@ require("dotenv").config();
 const app = express();
 
 // CORS configuration
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173, https://readrover.onrender.com ",
+];
 
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 // Middleware
 app.use(express.json());
-
 
 // Connect to MongoDB
 mongoose
