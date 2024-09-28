f4d4b3f79b4628ba7b78d3f546d18d7116ea3b0a
faizan841 <faizan200620@gmail.com>
2024-09-29 01:12:42 +0530

    change

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 // CORS configuration
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://127.0.0.1:5173, https://readrover.onrender.com ",
+  "http://127.0.0.1:5173, https://readrover.onrender.com",
 ];
 
 app.use(
